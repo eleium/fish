6a58cfd40daf94e074fb2a7c7fb45f25b74e5f25
eleium <422875640@qq.com>
2026-01-28 18:34:51 +0800

    增加变量与字符串操作

--- 2变量variable.py
+++ 2变量variable.py
@@ -1,9 +1,49 @@
-# dir(__builtins__) 显示所有python内置的函数名
-'''
-变量：variable'''
+# dir(__builtins__) 显示所有python内置的函数名，在python 的 IDE里
+
+# 变量：variable
+
 x = 3
 # x是变量名 。 = 是赋值操作。  3 是变量值。
-print(x)
+
+print(x)  # -->3
 # 变量名 不能以数字开头
 
-#这是新平台win10之后的测试.重新启用新的ssh之后。
+# python3以后还支持中文当变量名了
+
+记忆 = 887
+print(记忆)  # -->887
+
+name = '小甲鱼'  # 变量值为字符串要用括号括起来。否则python会认为 小甲鱼 是字符串。
+# 空字符串也要用''包裹，以示与None区别。
+print(name)  # -->小甲鱼
+
+name = '大乌龟'
+print(name)  # -->大乌龟  # 变量的值可以更改
+
+x = 3;
+y = 5  # 要在一行里给两个变量赋值可以用 ; 分开。也可以用更优雅的方法：
+x, y = 3, 5
+print(x, y)
+
+# 两个变量值互换：
+x, y = y, x
+print(x, y)  # -->5 3
+
+"""
+python 的变量名支持大小写严格区分
+不能以数字开头
+支持驼峰命名MyName 
+支持中文
+最后以下划线分隔开
+"""
+print('I love china')  # 单引号适用于无嵌套的情况。
+print("I love china,let's go!")  # 双引号适用于有嵌套的情况。文本两边引号要成对。
+# 三引号适用于复杂情况，多行。
+print("""I love china.
+I'm chinese.
+I'm from Dalian.
+""")
+# 用转义字符 \ 来表示你就是个单独的引号，而非与其他引号配对用的。
+print('\"life is short,you need \"python\"\"')  # -->"life is short,you need "python""
+print('I like apple,\nshe likes orange.')  # \n是换行转义符。
+
--- 3变量与字符串.py
+++ 3变量与字符串.py
@@ -1,9 +1,31 @@
-#变量与字符串：
+# 变量与字符串：
 print('d:\three\two\one\now')
 
-#反斜杠代表了转义，所以得到如下结果
+# 反斜杠代表了转义，所以得到如下结果，\t代表制表符，\o不知道，\n代表换行
 # d： hree  wo\one
-#ow
+# ow
 
-#这明显有问题，那么应该怎么解决呢???用n\转义符啊
-#现在这个项目对应的github.com/eleium/little_fish_online的main分支。可以推送到master分支吗？how?
+# 这明显有问题，那么应该怎么解决呢???用n\转义符啊
+print('d:/three/two/one/now')  # -->d:/three/two/one/now 这是用的绝对路径，不会出毛病。
+
+print('d:\\three\\two\\one\\now')  # -->d:\three\two\onw\now，把原来的\路径转义后，代表路径的意思，而非制表符啥的。
+
+print(r'd:\three\two\onw\now')  # -->d:\three\two\one\now  小写r,表示后面是原始字符串，\不再有效。
+print(r'how are you')  # -->how are you
+
+# triple quoted 三引号
+
+poetry = '''
+白日依山尽，
+黄河入海流。
+欲穷千里目，
+更上一层楼。
+'''
+print(poetry)
+
+# 字符串的加法
+print(520 + 1314)  # 这是两个数据相加，是数学相加-->1834
+
+print('520' + '1314')  # 这是两个字符串相加，是拼合字符串-->5201314
+print('I love you'*3)# -->I love you I love you I love you 字符串重复3次。配合\n使用
+
