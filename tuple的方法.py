7ea9402de90a34760ec9b8edad75e202719ed2be
eleium <422875640@qq.com>
2026-03-21 18:30:36 +0800

    字符串的方法（字符串不变）

---
+++ tuple的方法.py
@@ -0,0 +1,9 @@
+a=('hello','world',1,2,3,(1,2,3),5,5,(1,2,3))
+
+print(a.count('hello'))
+
+code='''
+    print('world hello')
+    print('hello world')
+    '''
+print(code)
---
+++ 字符串的方法count.py
@@ -0,0 +1,3 @@
+a=('hello','world',1,2,3,(1,2,3),5,5,(1,2,3))
+
+print(a.count('hello', 0,5))
