a2686963d86f0d874056aca562a1ceab19a8ef0b
eleium <422875640@qq.com>
2026-03-25 18:39:36 +0800

    字符串的方法分割（字符串不变）

--- 25字符串的方法后续3.py
+++ 25字符串的方法后续3.py
@@ -13,9 +13,10 @@ print('    两侧不要空白    '.strip())#---->两侧不要空白
 #chars=None:空白，该方法就是去除空白留白。
 
 print('--'*40)
-
+x='www.ilovefishc.com'
 print('www.python.com'.lstrip('wp'))#.python.com
 print('www.ilovefishc.com'.lstrip('wcom.'))#---->ilovefishc.com
 print('www.ilovefishc.com'.rstrip('wcom.'))#---->www.ilovefishc
 #strip()里面的参数chars='wcom.'代表的是5个独立的字符：w/c/o/m/.而不是一个字符串。
 #它会从左 右两侧，连续删除所有出现在这个集合里的字符，直到遇到第一个不在集合里的字符为止
+print(x)
