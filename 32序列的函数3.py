da3a39c981ce31fc1b33871df04cf9f2d35e4cfd
eleium <422875640@qq.com>
2026-04-04 11:58:06 +0800

    序列所用到的一些函数的用法。

---
+++ 32序列的函数3.py
@@ -0,0 +1,31 @@
+# map()函数：会根据提供的函数对指定的可迭代对象的每个元素进行运算，并将返回运算结果的迭代器。
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = ['how']
+zip(x, y, z)
+print(list(zip(x, y, z)))  # ---->[(1,2,'how')] 没有（2，5，'how'),也没有（3，6，'how'),
+# 因为zip()函数针对的是所给对象的元素，而z=['how']只有一个元素：'how'.
+# 要呈现多的元素的迭代器的结果，需要使用itertools.zip_longest()函数。首先要import itertools:
+import itertools
+
+print(list(itertools.zip_longest(x, y, z)))  # --->[(1,4,'how'),(2,5,None),(3,6,None)],2、5和3、6都出来了。缺少的用None填充。
+
+print(list(map(ord, 'fishC')))  # ord:求出一个字符的unicode编码---->[102, 105, 115, 104, 67]:fishC的每一个编码，返回列表。
+
+mapped = map(pow, [2, 3, 10], [5, 2, 3])  # pow(x,y)函数是求x的y次方。
+print(list(mapped))
+# map()函数与zip()函数一样，按传入的可迭代对象的元素最少的 来执行，其他的用None填充。
+print(list(map(max, [1, 2, 3], [4, 6], [0, 7, 8, 9])))  # --->[4，7]max:返回可迭代对象中的最大值。0就不管了.
+
+print('-' * 80)
+
+# filter()过滤器   函数会根据提供的函数对指定的迭代器对象的每个元素进行运算，并将运算结果为真的元素，以迭代器的形式返回。
+# 跟map()函数一样，filter()函数也是先传入一个函数当参数，然后对后面传入的可迭代对象操作。
+# 但是map()函数是用计算结果返回迭代器，
+# 而filter()函数是用计算结果为真的原迭代器的元素来返回一个迭代器。
+print(list(filter(str.islower, 'FishiC')))  # --->['i','s','h','i']islower:判读哪一个是小写，为True，两个i都列出了。
+
+# zip()聚合打包函数将多个序列打包成元
+# map()映射函数将函数映射到每个元素
+# filter()过滤函数根据条件过滤元素
---
+++ 33迭代器与可迭代对象.py
@@ -0,0 +1,40 @@
+"""
+一个迭代器必然是一个可迭代对象，但是，可迭代对象是可以重复操作，而迭代器只能用一次。
+"""
+
+mapped = map(ord, 'fishC')
+for each in mapped:
+    print(each)
+
+print(list(mapped))
+print(mapped)
+
+x = [1, 2, 3, 4, 5]
+y = iter(x)
+print(y)  # --> <list_iterator object at 0x0000020E0EA0EA90>  列表迭代器，内存地址：0x0000020E0EA0EA90
+print(type(x))  # ---> <class 'list'>  类型：列表
+print(type(y))  # ---> <class 'list_iterator'>  类型： 列表类的迭代器
+
+# next()函数：把迭代器的内容元素逐个提取出来：运行一次，提取一个元素。
+print(next(y))  # ---> 1
+print(next(y))  # ---> 2
+print(next(y))  # ---> 3
+print(next(y))  # ---> 4
+print(next(y))  # ---> 5
+
+# 运行一次，没有元素了，报错：这是个异常，异常是可控的，往往是故意做出的。如果是错误，那是不可控的。
+# print(next(y))  # ---> Traceback (most recent call last):
+#  File "D:/python/python_code/33迭代器与可迭代对象.py", line 27, in <module>
+#    print(next(y))
+# StopIteration
+
+#如果不想抛出异常，可以用next()函数的第二个参数：
+
+z=iter(x)#因为迭代器 y=iter(x)是一次性的，所以要重新定义一个迭代器。
+print(next(z))#--->1
+print(next(z))#--->2
+print(next(z))#--->3
+print(next(z))#--->4
+print(next(z))#--->5
+#所有元素已经都提取完毕，此时如果不想抛出异常，可以给next()函数加上第二个参数：
+print(next(z,'没有元素啦'))#--->没有元素啦
