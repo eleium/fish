d960c56ac40b71133ca294bd1f05ab42b7eaf3c9
eleium <422875640@qq.com>
2026-04-06 18:07:48 +0800

    用update()函数增加原字典的项

--- 35字典（下）.py
+++ 35字典（下）.py
@@ -5,5 +5,9 @@ d['f'] = 100
 print(d)
 
 # update()
-d.update(ishC=230)
-print(d)  # -->{'f':100,'i':None,'s':None,'h':None,'C':230} 增加了一个键值对：ishC:230
+d.update(ishC=230)  # 添加一个键值对，字典里原来没有该键值对。
+print(d)  # -->{'f':100,'i':None,'s':None,'h':None,'C':None,'ishC':230}
+# 增加了一个键值对：ishC:230
+
+d.update({"i": 100, "s": 200})
+print(d)  # -->{'f':100,'i':100,'s':200,'h':None,'C':None,'ishC':230}
