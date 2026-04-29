cccaab86c4fb816ba9e48490f25a82808d23f376
eleium <422875640@qq.com>
2026-04-29 19:28:27 +0800

    重新安装了win10

---
+++ 52永久储存（中）路径.py
@@ -0,0 +1,8 @@
+# pathlib文档
+from pathlib import Path  # 表示从模块pathlib里面，单独导入Path。这种导入方式，后面引用的时候，不需要前缀模块名。
+
+print(Path.cwd())  # -->D:\python_learning\fish 获取当前的工作目录路径。
+# 创建一个路径对象：
+p=Path(d:/python_learning/fish)
+q=
+# 我们有一个文件名叫：fishc_python_test.py,把它赋值给一个对象 q。!!
