0ef3eae2a08922db21854b1dc66f9a4d59be06db
eleium <422875640@qq.com>
2026-05-04 22:16:18 +0800

    改到visaul studio code，看看这里跟pycharm的感觉哪个舒服。

--- 53永久储存（下）.py
+++ 53永久储存（下）.py
@@ -1,1 +1,9 @@
 # with 语句 和上下文管理器。with语句不需要打开文件后，用close()方法关闭文件。
+#with语句的结构： with open('路径','w',encondng='utf-8') as f:
+f = open('d:/python_learning/fish/elephant/tiger.txt', 'w', encoding='utf-8')
+#比较open()方法和with语句：
+with open('d:/python_learning/fish/elephant/tiger.txt', 'w', encoding='utf-8') as h:
+    h.write('hello world,do you love python?')
+# with语句会自动调用close()方法，不用手动调用。
+# with语句会自动处理异常，不用手动处理。
+# with语句会自动处理文件打开和关闭
