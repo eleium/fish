daa3f31f42f161217c3c1d9ccea26fcc9a2f881e
eleium <422875640@qq.com>
2026-05-04 20:48:13 +0800

    永久储存的方法操作

--- 52.1永久储存（中）路径目录操作.py
+++ 52.1永久储存（中）路径目录操作.py
@@ -6,8 +6,18 @@ m = Path('new_fishC.txt')  # 只是创建路径对象，不会创建真实文件
 # 如果是m.touch(),那么创建的是一个空文件。
 
 mkdir = Path('d:/python_learning/new_fishC.txt')  # 也只是创建路径对象，不会创建真实文件。而且创建的是文件夹对象
-
 m.mkdir()  # 创建了在d:/python_learning/fish 即当前文件目录下的一个文件夹：new_fishC.txt
 # m.mkdir('d:/python_learning')  报错：mkdir()不接受路径为参数，只接受 parents:True 或者 exist_ok:True这两个参数。
 
-m.replace('no_fishc.txt')  # 替换文件文件夹的名字：no_fishc.txt
+# 删除文件夹用rmdir() 、 用unlink()方法删除文件
+m.rmdir()
+c = Path('test.py')
+c.unlink()
+
+# m.replace('no_fishc.txt')  # 替换文件文件夹的名字：no_fishc.txt。前面用rmdir()方法删除了文件夹，所以这里会报错。
+
+
+# 功能强大的glob()方法：查找功能
+# 查找当前目录下的下一级目录中，所有后缀为.py的文件
+p = Path('.')  # 表示当前目录
+p.glob('*.py')
---
+++ 52.2永久储存（中）路径的查找功能.py
@@ -0,0 +1,31 @@
+# 用glob()方法查找功能
+
+from pathlib import Path
+
+from pprint import pprint
+
+# 查找当前目录的特定后缀文件：
+p = Path('.')  # 当前目录
+p.glob('*.txt')
+pprint(list(p.glob('*.txt')))  # --->[]
+p.glob('*.py')
+pprint(list(p.glob('*.py')))
+
+# 查找当前目录的下一级目录的特定后缀文件：
+pprint(list(p.glob('./.venv/*.cfg')))
+
+print('_' * 88)
+
+# 查找当前目录及当前目录下的所有子目录:即查找当前目录以及所有递归子目录下的文件和文件夹
+# pprint(list(p.glob('**./')))
+"""
+'**./' 这个模式是无效的。错误信息明确指出：'**' 只能作为完整的路径组件使用，不能和 ./ 连在一起。
+glob 模式的正确用法
+** 表示递归匹配所有子目录
+* 表示匹配任意字符（不包括路径分隔符）
+** 必须单独使用，前后应该有 / 或作为路径的一部分"""
+
+pprint(list(p.glob('**/')))
+print('-.-' * 88)
+# 查找当前及递归子目录下的所有.py文件
+pprint(list(p.glob('**/*.py')))
