ad8c32b2fd344cf6f71e47941979e8ed9b2b7777
eleium <422875640@qq.com>
2026-08-15 16:16:12 +0800

    ok

--- 字符串的方法count.py
+++ 字符串的方法count.py
@@ -1,3 +1,5 @@
-a=('hello','world',1,2,3,(1,2,3),5,5,(1,2,3))
+a = ("hello", "world", 1, 2, 3, (1, 2, 3), 5, 5, (1, 2, 3))
 
-print(a.count('hello', 0,5))
+
+print(a.count("hello"))
+print(a.count(0.5))
